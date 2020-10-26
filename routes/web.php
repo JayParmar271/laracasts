8923c7504f6e6d633b453fefbe703d28b7b4f1f0
jayparmar271 <jayparmar271@gmail.com>
2020-10-27 01:08:29 +0530

    5.3 Laravel Caching with redis

--- routes/web.php
+++ routes/web.php
@@ -14,21 +14,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-function remember($key, $minutes, $callback)
-{
-    if ($value = Redis::get($key)) {
-        return json_decode($value);
-    }
-
-    $value = $callback();
-
-    Redis::setex($key, $minutes, $value);
-
-    return $value;
-}
-
 Route::get('/', function () {
-    return remember('articles.all', 60*60, function () {
+    return Cache::remember('articles.all', 60*60, function () {
         return App\Models\Article::all();
     });
 });
